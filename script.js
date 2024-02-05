72be8b48135e06d81ad01790bbc5abb6c34f58b1
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-02-05 12:13:06 +0530

    Some features added

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
+let intervalBGC;
 
+// Random Color Generator Function
+function randomBGC() {
+    const hexValue = "0123456789ABCDEF";
+    let color = "#";
+    for(let i=0; i<=5; i++) {
+        const randomValue = Math.floor(Math.random()*16);
+        color += hexValue[randomValue];
+    }
+    return color;
+}
+
+// Function for changing BGC in every second
+function startChangingBGC() {
+    intervalBGC = setInterval(()=>{
+        const bgcolor = randomBGC();
+        document.body.style.backgroundColor = bgcolor;
+    }, 1000);
+}
+
+// Function to stop changing BG color
+function stopChangingBGC() {
+    clearInterval(intervalBGC);
+}
+
+// Start Button Click Event
+startBtn.addEventListener("click", startChangingBGC);
+
+// Stop Button Click Event
+stopBtn.addEventListener("click", stopChangingBGC);
